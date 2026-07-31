5f15fb2c1001ecd8b76beddd44a4b53a4e266f95
Spo0ochy <300404370+Spo0ochy@users.noreply.github.com>
2026-07-31 20:23:37 +0200

    Fix the display
    Set width=height=500px
    Adapt the size of .pixel with size value
    Adjust color for better visualization

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const display = document.querySelector(".display")
 
-let size = 16;
+let size = 5;
+let ratio = 1 / size;
 
 const createPixels = (size) => {
     for (let i = 0; i < size; i++) {
         const linePixel = document.createElement("div");
+        linePixel.classList.add("linePixel");
 
         display.append(linePixel);
 
         for (let j = 0; j < size; j++) {
             const pixel = document.createElement("div");
+            pixel.style.height = (ratio * 500) + "px";
+            pixel.style.width = (ratio * 500) + "px";
 
             pixel.classList.add("pixel");
             linePixel.append(pixel);
